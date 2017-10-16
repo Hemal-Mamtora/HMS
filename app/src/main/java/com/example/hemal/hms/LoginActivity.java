a82f7625fa09d35ca57ebc3786486919660cb021
Hemal Mamtora <hemalmamtora1@gmail.com>
2017-10-16 19:57:05 +0530

    added db

--- app/src/main/java/com/example/hemal/hms/LoginActivity.java
+++ app/src/main/java/com/example/hemal/hms/LoginActivity.java
@@ -33,6 +33,17 @@ import java.util.ArrayList;
 import java.util.List;
 import android.content.Intent;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import static android.Manifest.permission.READ_CONTACTS;
 
 /**
